c91deb40ee359df941289e70c1b65b6a7f70628d
Souphis <gbartyzel@hotmail.com>
2019-12-29 21:44:52 +0100

    List of changes: * Adjust ReplayBuffer for dict states * Adjust Rollout for dict states

--- mobile_robot_rl/common/memory.py
+++ mobile_robot_rl/common/memory.py
@@ -1,5 +1,7 @@
+import copy
 from collections import namedtuple
 from typing import Any
+from typing import Dict
 from typing import Sequence
 from typing import Tuple
 from typing import Union
@@ -7,7 +9,11 @@ from typing import Union
 import numpy as np
 import torch
 
-_DATA = Union[np.ndarray, torch.Tensor]
+_DATA_TYPE = Union[np.ndarray, torch.Tensor]
+_DIM_TYPE = Union[int, Sequence[int]]
+_DICT_DATE_TYPE = Union[Dict[str, _DATA_TYPE], _DATA_TYPE]
+_DICT_DIM_TYPE = Union[_DIM_TYPE, Dict[str, _DIM_TYPE]]
+_TRANSITION = Tuple[_DICT_DATE_TYPE, np.ndarray, _DICT_DATE_TYPE, float, bool]
 
 Batch = namedtuple('Batch', 'state action reward next_state mask')
 
@@ -15,11 +21,14 @@ Batch = namedtuple('Batch', 'state action reward next_state mask')
 class RingBuffer:
     TORCH_BACKEND = False
 
-    def __init__(self, capacity: int, dimension: Union[int, Sequence[int]]):
+    def __init__(self,
+                 capacity: int,
+                 dimension: _DIM_TYPE,
+                 dtype: np.dtype):
         self._size = 0
         self._capacity = capacity
         self._dimension = self._to_tuple(dimension)
-        self._container = np.zeros((capacity,) + self._dimension)
+        self._container = np.zeros((capacity,) + self._dimension, dtype=dtype)
 
     def add(self, value: Any):
         self._container[self._size, :] = value
@@ -27,7 +36,7 @@ class RingBuffer:
 
     def sample(self,
                idx: Union[Sequence[int], torch.Tensor],
-               device: torch.device = torch.device('cpu')) -> _DATA:
+               device: torch.device = torch.device('cpu')) -> _DATA_TYPE:
         batch = self._container[idx]
         if RingBuffer.TORCH_BACKEND:
             return torch.from_numpy(batch).float().to(device)
@@ -41,15 +50,15 @@ class RingBuffer:
         return self._size
 
     @property
-    def first(self) -> _DATA:
+    def first(self) -> _DATA_TYPE:
         return self._container[0]
 
     @property
-    def end(self) -> _DATA:
+    def end(self) -> _DATA_TYPE:
         return self._container[-1]
 
     @property
-    def data(self) -> _DATA:
+    def data(self) -> _DATA_TYPE:
         return self._container
 
     @staticmethod
@@ -65,11 +74,18 @@ class RingBuffer:
         return self._container[item]
 
 
+_STATE_BUFFER = Union[RingBuffer, Dict[str, RingBuffer]]
+
+
 class ReplayMemory(object):
     def __init__(self,
                  capacity: int,
                  state_dim: Union[int, Sequence[int]],
                  action_dim: Union[int, Sequence[int]],
+                 state_dtype: Union[Dict[str, np.dtype], np.dtype],
+                 action_dtype: np.dtype = np.float32,
+                 reward_dtype: np.dtype = np.float32,
+                 terminal_dtype: np.dtype = np.float32,
                  combined: bool = False,
                  torch_backend: bool = False):
         RingBuffer.TORCH_BACKEND = torch_backend
@@ -77,23 +93,29 @@ class ReplayMemory(object):
         self._combined = combined
         self._size = 0
 
-        self._state_buffer = RingBuffer(capacity, state_dim)
-        self._action_buffer = RingBuffer(capacity, action_dim)
-        self._reward_buffer = RingBuffer(capacity, 1)
-        self._next_state_buffer = RingBuffer(capacity, state_dim)
-        self._mask_buffer = RingBuffer(capacity, 1)
+        if isinstance(state_dim, dict):
+            self._state_buffer = {k: RingBuffer(capacity, v, state_dtype[k])
+                                  for k, v in state_dim.items()}
+        else:
+            self._state_buffer = RingBuffer(capacity, state_dim, state_dtype)
+        self._action_buffer = RingBuffer(capacity, action_dim, action_dtype)
+        self._reward_buffer = RingBuffer(capacity, 1, reward_dtype)
+        self._next_state_buffer = copy.deepcopy(self._state_buffer)
+        self._mask_buffer = RingBuffer(capacity, 1, terminal_dtype)
 
     def push(self,
-             state: _DATA,
-             action: _DATA,
+             state: _DICT_DATE_TYPE,
+             action: _DATA_TYPE,
              reward: Union[float, torch.Tensor],
-             next_state: Union[float, torch.Tensor],
+             next_state: _DICT_DATE_TYPE,
              terminal: Union[float, bool, torch.Tensor]):
         self._size = min(self._size + 1, self._capacity)
-        self._state_buffer.add(state)
+
+        self._state_buffer = self._push_state(self._state_buffer, state)
         self._action_buffer.add(action)
         self._reward_buffer.add(reward)
-        self._next_state_buffer.add(next_state)
+        self._next_state_buffer = self._push_state(self._next_state_buffer,
+                                                   next_state)
         self._mask_buffer.add(1.0 - terminal)
 
     def sample(self,
@@ -106,10 +128,11 @@ class ReplayMemory(object):
             idxs = np.append(idxs, np.array(self.size - 1, dtype=np.int32))
 
         batch = Batch(
-            state=self._state_buffer.sample(idxs, device),
+            state=self._sample_state(self._state_buffer, idxs, device),
             action=self._action_buffer.sample(idxs, device),
             reward=self._reward_buffer.sample(idxs, device),
-            next_state=self._next_state_buffer.sample(idxs, device),
+            next_state=self._sample_state(self._next_state_buffer, idxs,
+                                          device),
             mask=self._mask_buffer.sample(idxs, device))
 
         return batch
@@ -126,53 +149,91 @@ class ReplayMemory(object):
         terminal = self._mask_buffer[item]
         return state, action, reward, next_state, terminal
 
+    @staticmethod
+    def _push_state(buffer: _STATE_BUFFER,
+                    data: _DICT_DATE_TYPE) -> _STATE_BUFFER:
+        if isinstance(buffer, dict):
+            for key, value in data.items():
+                buffer[key].add(value)
+        else:
+            buffer.add(data)
+        return buffer
+
+    @staticmethod
+    def _sample_state(buffer: _STATE_BUFFER,
+                      idxs: np.ndarray,
+                      device: torch.device) -> _DICT_DATE_TYPE:
+        if isinstance(buffer, dict):
+            return {k: v.sample(idxs, device) for k, v in buffer.items()}
+        return buffer.sample(idxs, device)
+
 
 class Rollout:
     def __init__(self,
                  capacity: int,
-                 state_dim: Union[int, Tuple[int, int]],
+                 state_dim: _DICT_DIM_TYPE,
                  action_dim: int,
-                 discount_factor: float):
+                 discount_factor: float,
+                 state_dtype: Union[Dict[str, np.dtype], np.dtype],
+                 action_dtype: np.dtype = np.float32):
         self._size = 0
         self._capacity = capacity
         self._discount_factor = discount_factor
 
-        if isinstance(state_dim, tuple):
-            self._state_buffer = np.zeros((capacity, ) + state_dim)
+        if isinstance(state_dim, dict):
+            self._state_buffer = {k: self._create_buffer(v, state_dtype[k])
+                                  for k, v in state_dim.items()}
         else:
-            self._state_buffer = np.zeros((capacity, state_dim))
-        self._action_buffer = np.zeros((capacity, action_dim))
-        self._reward_buffer = np.zeros((capacity, 1))
+            self._state_buffer = self._create_buffer(state_dim, state_dtype)
+        self._action_buffer = self._create_buffer(action_dim, action_dtype)
+        self._reward_buffer = self._create_buffer(1)
 
     @property
-    def ready(self):
+    def ready(self) -> int:
         return self._size == self._capacity
 
     def push(self,
-             state: _DATA,
-             action: _DATA,
-             reward: Union[float, torch.Tensor]):
-        self._state_buffer = self._add(self._state_buffer, state)
+             state: _DICT_DATE_TYPE,
+             action: _DATA_TYPE,
+             reward: Union[float, np.ndarray]):
+        if isinstance(self._state_buffer, dict):
+            for key in self._state_buffer.keys():
+                self._state_buffer[key] = self._add(
+                    self._state_buffer[key], state[key])
+        else:
+            self._state_buffer = self._add(self._state_buffer, state)
         self._action_buffer = self._add(self._action_buffer, action)
         self._reward_buffer = self._add(self._reward_buffer, reward)
         self._size = min(self._size + 1, self._capacity)
 
-    def get_transition(self, state, action, reward, next_state, done):
+    def get_transition(self,
+                       state: _DICT_DATE_TYPE,
+                       action: _DATA_TYPE,
+                       reward: Union[np.ndarray, float],
+                       next_state: _DICT_DATE_TYPE,
+                       done: Union[np.ndarray, bool]) -> _TRANSITION:
         self.push(state, action, reward)
         if self._size == self._capacity:
             cum_reward = self._compute_cumulative_reward()
-            state = self._state_buffer[0]
+            if isinstance(self._state_buffer, dict):
+                state = {key: val[0] for key, val in self._state_buffer.items()}
+            else:
+                state = self._state_buffer[0]
             action = self._action_buffer[0]
             return state, action, cum_reward, next_state, done
         return None
 
     def reset(self):
-        self._state_buffer = np.zeros(self._state_buffer.shape)
+        if isinstance(self._state_buffer, dict):
+            self._state_buffer = {key: np.zeros(self._state_buffer[key].shape)
+                                  for key in self._state_buffer.keys()}
+        else:
+            self._state_buffer = np.zeros(self._state_buffer.shape)
         self._action_buffer = np.zeros(self._action_buffer.shape)
         self._reward_buffer = np.zeros(self._reward_buffer.shape)
         self._size = 0
 
-    def _compute_cumulative_reward(self):
+    def _compute_cumulative_reward(self) -> float:
         cum_reward = 0
         for t in range(self._capacity):
             cum_reward += self._discount_factor ** t * self._reward_buffer[t]
@@ -184,16 +245,9 @@ class Rollout:
         buffer[-1] = value
         return buffer
 
-
-if __name__ == '__main__':
-    mem = ReplayMemory(100000, (4, 9), 2)
-
-    for i in range(100000):
-        state = np.random.rand(4, 9)
-        action = np.random.rand(2)
-        rewrd = 1.0
-        next_state = np.random.rand(4, 9)
-        mem.push(state, action, rewrd, next_state, False)
-
-    batch = mem.sample(32)
-    print(batch.state)
+    def _create_buffer(self,
+                       dim: int,
+                       dtype: np.dtype = np.float32) -> _DICT_DATE_TYPE:
+        if isinstance(dim, tuple):
+            return np.zeros((self._capacity,) + dim, dtype=dtype)
+        return np.zeros((self._capacity, dim), dtype=dtype)
